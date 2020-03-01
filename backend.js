c2a8b51be1e68f87b2b2aaddbfa948c5bedae940
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-03-01 20:48:37 +0300

    add map

--- backend.js
+++ backend.js
@@ -1,42 +1,29 @@
 const express = require('express');
-//const request = require('request');
+const request = require('request');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-//let url = "https://api.ibb.gov.tr/ispark/Park"
+let url = "https://api.ibb.gov.tr/ispark/Park"
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/district', (req, res) => {
-    console.log(req.body);
-    res.send(
-        `Received a POST request. Selected district: ${req.body.post}`,
-    );
-});
+app.get('/api/data', (req, res) => {
+    data = [];
 
-app.post('/carpark', (req, res) => {
-    console.log(req.body);
-    res.send(
-        `Received a POST request. Selected Car Park: ${req.body.post}`,
-    );
-});
-
-/*
-app.get('/api/districts', (req, res) => {
     request({
         url: url,
         json: true
     }, function (error, response, body) {
             if (!error && response.statusCode === 200) {
                 for (var i in body) {
-                    console.log(body[i].Ilce);
+                    data.push(body[i])
                 }
             }
+        res.json(data);
     })
 })
-*/
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 class App extends Component {
   state = {
     data: '',
     district: '',
-    carpark: '',
-    districtResponse: '',
-    carparkResponse: ''
+    ParkID: 0
   };
 
   async componentDidMount() {
-    const url = "https://api.ibb.gov.tr/ispark/Park";
-    const response = await fetch(url);
-
-    const data = await response.json();
-
-    this.setState({ data: data} )
-  }
-
-  handleDistrictSelection = async e => {
-
-    e.preventDefault();
-
-    const response = await fetch('/district', {
-      method: 'POST',
+    const response = await fetch('/api/data', {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.district }),
+      }
     });
 
-    const body = await response.text();
-    
-    this.setState({ districtResponse: body });
+    const body = await response.json();
 
-  };
-
-  handleCarParkSelection = async e => {
-
-    e.preventDefault();
-
-    const response = await fetch('/carpark', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.carpark })
-    });
-
-    const body = await response.text();
-
-    this.setState({ carparkResponse: body });
-
+    this.setState({ data: body });
   }
 
   createDistrictOptions() {
     let districtList = [];
 
     for (var i in this.state.data) {
-      districtList.push(<option key={i} value={this.state.data[i].Ilce}>{this.state.data[i].Ilce}</option>)
+      districtList.push(this.state.data[i].Ilce);
     }
-    return districtList;
+
+    districtList = [...new Set(districtList)];
+
+    let uniqueDistrictOptions = [];
+
+    for (var j in districtList) {
+      uniqueDistrictOptions.push(<option key={j} value={districtList[j]}>{districtList[j]}</option>)
+    }
+
+    return uniqueDistrictOptions;
   }
 
   createCarParkOptions() {
     let carParkList = [];
 
     for (var i in this.state.data) {
-
       if (this.state.data[i].Ilce === this.state.district)
-        carParkList.push(<option key={i} value={this.state.data[i].ParkAdi}>{this.state.data[i].ParkAdi}</option>)
+        carParkList.push(<option key={i} value={this.state.data[i].ParkID}>{this.state.data[i].ParkAdi}</option>)
     }
 
     return carParkList;
@@ -80,50 +55,127 @@ class App extends Component {
     let capacity = 0;
 
     for (var i in this.state.data) {
-
-      if (this.state.data[i].ParkAdi === this.state.carpark)
+      if (Number(this.state.data[i].ParkID) === Number(this.state.ParkID)) {
         capacity = this.state.data[i].BosKapasite;
+      }
     }
 
     return capacity;
   }
 
+  getUsedParkingSpaces() {
+    let capacity = 0;
+    let freeCapacity = 0;
+
+    for (var i in this.state.data) {
+
+      if (Number(this.state.data[i].ParkID) === Number(this.state.ParkID)) {
+        freeCapacity = this.state.data[i].BosKapasite;
+        capacity = this.state.data[i].Kapasitesi;
+      }
+    }
+
+    return capacity - freeCapacity;
+  }
+
+  getLatitude() {
+    let lat = 40.96;
+
+    for (var i in this.state.data) {
+
+      if (Number(this.state.data[i].ParkID) === Number(this.state.ParkID)) {
+        lat = this.state.data[i].Latitude;
+      }
+    }
+
+    return lat;
+  }
+
+  getLongitude() {
+    let lon = 29.10;
+
+    for (var i in this.state.data) {
+
+      if (Number(this.state.data[i].ParkID) === Number(this.state.ParkID)) {
+        lon = this.state.data[i].Longitude;
+      }
+    }
+
+    return lon;
+
+  }
+
   render() {
+    const CarParkLocation = withScriptjs(withGoogleMap((props) =>
+      <GoogleMap
+        defaultZoom={ 15 }
+        defaultCenter={{ lat: props.lat, lng: props.lon }}
+      >
+        { props.showMarker && 
+          <Marker position={{ lat: props.lat, lng: props.lon }} />
+        }
+      </GoogleMap>
+    ))
+
+    const DistrictSelectionForm = () =>
+        <form>
+            <p>
+              <strong>Districts of Car Parks:</strong>
+            </p>
+            <select
+              value={this.state.district}
+              onChange={e => this.setState({ district: e.target.value })}>
+                  <option value="">Select a district</option>
+                  {this.createDistrictOptions()}
+            </select>
+            
+        </form>
+
+    const CarParkSelectionForm = () => 
+        <form>
+            <p>
+              <strong>Car Parks:</strong>
+            </p>
+            <select
+              value={this.state.ParkID}
+              onChange={e => this.setState({ ParkID: e.target.value })}>
+                  {!this.state.district && <option value="">Please select a district first</option>}
+                  {this.state.district && <option value="">Select a car park</option>}
+                  {this.createCarParkOptions()}
+            </select>
+            
+        </form>
+
     return (
       <div className="App">
         <header className="App-header">
           Ispark Car Parks
         </header>
 
-        <form onSubmit={this.handleDistrictSelection}>
-            <p>
-              <strong>Districts of Car Parks:</strong>
-            </p>
-            <select value={this.state.district} onChange={e => this.setState({ district: e.target.value })}>
-              {this.createDistrictOptions()}
-            </select>
-            <button type="submit">Select District</button>
-        </form>
+        <DistrictSelectionForm/>
 
-        <p>{this.state.districtResponse}</p>
-
-        <form onSubmit={this.handleCarParkSelection}>
-            <p>
-              <strong>Car Parks:</strong>
-            </p>
-            <select value={this.state.carpark} onChange={e => this.setState({ carpark: e.target.value })}>
-              {this.createCarParkOptions()}
-            </select>
-            <button type="submit">Select Car Park</button>
-        </form>
-
-        <p>{this.state.carparkResponse}</p>
+        <CarParkSelectionForm/>
 
         <p>
           <strong>Free Parking Spaces:</strong>
         </p>
-        {this.getFreeParkingSpaces()}
 
+        {this.getFreeParkingSpaces()} ({this.getUsedParkingSpaces()} used)
+
+        <p>
+          <strong>Location of the Car Park:</strong>
+        </p>
+
+        <CarParkLocation
+          showMarker="true"
+          lon={Number(this.getLongitude())}
+          lat={Number(this.getLatitude())}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: '100%' }} />}
+          containerElement={<div style={{ height: '500px' }} />}
+          mapElement={<div style={{ height: '100%' }} />}
+        />
+      
       </div>
     );
   }
