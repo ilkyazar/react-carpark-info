2a17f86dc6e764855ad8b274007fb0dab0fc807d
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-03-01 21:46:42 +0300

    update index and manifest

---
+++ frontend/build/precache-manifest.d8bb457e4ca46f4255827d57955f8b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a81a54da3a51f15e054b1a2b3e1f8da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "965bc5e339f33f158fa6",
+    "url": "/static/css/main.cbe4b799.chunk.css"
+  },
+  {
+    "revision": "02fa376ebe5121310a89",
+    "url": "/static/js/2.98d93490.chunk.js"
+  },
+  {
+    "revision": "577ef60f73636c6e95b316a24bc4f83f",
+    "url": "/static/js/2.98d93490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965bc5e339f33f158fa6",
+    "url": "/static/js/main.32bd10b8.chunk.js"
+  },
+  {
+    "revision": "fe238525b9b2babc456b",
+    "url": "/static/js/runtime-main.26b2c054.js"
+  }
+]);
