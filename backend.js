1f7064337b33292049c6eda80003be28b4941611
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-03-01 21:53:14 +0300

    modify backend

--- backend.js
+++ backend.js
@@ -26,12 +26,5 @@ app.get('/api/data', (req, res) => {
     })
 })
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')));
-      
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-    });
-  }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
